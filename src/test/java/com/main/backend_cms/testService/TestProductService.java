b60a6ac0ef42e6a8ce5154906cffdec639b8e658
Notdakon Chantarat <69079640+NtdChnn@users.noreply.github.com>
2023-02-18 15:16:19 +0700

    add testproduct case

--- src/test/java/com/main/backend_cms/testService/TestProductService.java
+++ src/test/java/com/main/backend_cms/testService/TestProductService.java
@@ -1,9 +1,12 @@
 package com.main.backend_cms.testService;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.main.backend_cms.model.Products;
 import com.main.backend_cms.service.ProductService;
 
 @SpringBootTest
@@ -13,7 +16,14 @@ public class TestProductService {
     private ProductService productService;
     
     @Test
-    void should_return_createsuccess_when_given_product(){
-        
+    void should_return_createsuccess_when_call_createproduct_given_product(){
+        Products product = new Products("pName", 
+                                99.99, 
+                                100, 
+                                "pName is a very good product", 
+                                "catid", 
+                                "pNamePic");
+        String data = productService.createProduct(product);
+        assertEquals(data, "Create success");
     }
 }
