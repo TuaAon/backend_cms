8edc1b44e4103e08a7cf7581250720dbc3b2d92f
Notdakon Chantarat <69079640+NtdChnn@users.noreply.github.com>
2023-02-18 14:25:57 +0700

    fixCreateProduct

--- src/main/java/com/main/backend_cms/model/Products.java
+++ src/main/java/com/main/backend_cms/model/Products.java
@@ -1,7 +1,6 @@
 package com.main.backend_cms.model;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
